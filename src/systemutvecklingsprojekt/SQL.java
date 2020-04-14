ab5f3c68f607acc700e9f083ed5a29fc3740eb22
Marcus Skårud <marcus@skarud.se>
2020-04-14 15:00:46 +0200

    Update SQL.java

--- src/systemutvecklingsprojekt/SQL.java
+++ src/systemutvecklingsprojekt/SQL.java
@@ -49,6 +49,30 @@ public class SQL {
 
     }
     
+    public static ArrayList<ArrayList<String>> lasBlogginlagg(Connection db) throws SQLException{
+        ArrayList<ArrayList<String>> retur = new ArrayList<ArrayList<String>>();
+        ArrayList<String> arr = new ArrayList<String>();
+    
+        
+        String sql = "SELECT * FROM Blogginlagg";
+
+        Statement statement  = db.createStatement();
+        ResultSet resultat    = statement.executeQuery(sql);
+
+            while (resultat.next()) {
+                arr.add(resultat.getString("Rubrik"));
+                arr.add(resultat.getString("Text"));
+                retur.add(arr);        
+                
+            }
+        
+        
+        
+        
+        return retur;
+    }
+    
+    
     public static ArrayList<String> loggaIn(Connection db, String epost, String losenord){
         String sql = "";
         ArrayList<String> resultat = null;
